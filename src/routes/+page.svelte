b66491f1446e74e9d1501112e42bd9bdf331b089
Geonwoo Lee <geonwoo.lee@unist.ac.kr>
2023-06-15 18:57:14 +0900

    :tada: intial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="container">
+    <div class="navbar">
+        <a href="/" class="btn btn-ghost normal-case text-xl">SFA Friction</a>
+    </div>
+</div>
+
+<slot />
